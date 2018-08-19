0ae986e762d01efb217126989b0c394eff42739f
mbekins <milobekins@gmail.com>
2018-08-19 10:07:55 -0700

    test

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 USE burgers_db;
 
-INSERT INTO burgers (burger_name) VALUE (Cheeseburger);
-INSERT INTO burgers (burger_name) VALUE (Bacon Cheeseburger);
-INSERT INTO burgers (burger_name) VALUE (ChiliBurger);
+INSERT INTO burgers (burger_name) VALUE ("Cheeseburger");
+INSERT INTO burgers (burger_name) VALUE ("Bacon Cheeseburger");
+INSERT INTO burgers (burger_name) VALUE ("ChiliBurger");
